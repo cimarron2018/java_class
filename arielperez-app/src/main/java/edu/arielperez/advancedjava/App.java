2a2e0b58fb6107126e3e72a1802b93af1d23ec59
AP <kokoroman@rocketmail.com>
2018-09-09 23:59:25 -0400

    Initial commit
    First commit.

---
+++ arielperez-app/src/main/java/edu/arielperez/advancedjava/App.java
@@ -0,0 +1,13 @@
+package edu.arielperez.advancedjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
